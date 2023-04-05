20ac219822f464cd329e432b1e094291dba7db69
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-04-05 10:27:13 -0400

    donde up to removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,26 +2,32 @@ const removeFromArray = function(arrayInput,removeThis)
     {
         let arrayOut = Array.from(arrayInput);
         console.log(arrayOut);
-        let removeThisArray = Array.from(removeThis);
-        console.log(removeThisArray);
-        // removeThisLength = (removeThisArray.length);
-        // console.log(removeThisLength);
-        // console.log(removeThis[0]);
-        // console.log(removeThis[1]);
-        // console.log(removeThis[2]);
-            // for (i=o; i>2;i++)
-            // {
-            //     positionToRemove = arrayOut.indexOf(removeThis)
-            //     console.log(positionToRemove);
-            //     arrayOut.splice(positionToRemove,1)
-            // }
+        let removeThisArray = Array.from(arguments);
+        lengthArguments = arguments.length;
+        console.log("lenght",lengthArguments);
+        removeThisArray.splice(0,1);
+        console.log("items to remove",removeThisArray);
+        
+        for (i=0; i<(lengthArguments-1); i++)
+            {
+                if (arrayOut.indexOf(removeThisArray[i]) < 0)
+                            {
+                                continue
+                            }
+                            else 
+                            {
+                             arrayOut.splice(arrayOut.indexOf(removeThisArray[i]), 1)
+                            }             
+            }
 
-  
+        console.log(arrayOut)
+        return arrayOut;
+ 
     };
 
 
 
-    removeFromArray([1,2,3,4,5,6,7,8,9,10],3,6)
+    removeFromArray([1,2,3,4,5,6,7,8,9,10],3, 4, "hdh")
 
 
     // const removeFromArray = function(arrayInput,removeThis) 
