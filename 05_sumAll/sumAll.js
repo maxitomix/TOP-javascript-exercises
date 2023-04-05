0505d0733027cc2d9825f143a090e57c2f80281a
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-04-05 11:50:43 -0400

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,38 @@
-const sumAll = function() {
+const sumAll = function(a,b) 
+{
+let sum = 0;
+let start = 0;
+let end = 0;
+let fail;
+if (a<0 || b<0 || !Number.isInteger(a) || !Number.isInteger(b))
+    {
+    fail = "ERROR"
+    console.log(fail);
+    return fail;
+    }
+    else
+    {
+        if (a>b)
+            {start = b;
+            end = a;  
+            }
+            else
+            {
+            start = a;
+            end = b;
+            }
+        
+                for (i=start; i<=end; i++)
+                {
+                sum += i;
+                }
+            console.log(sum);
+            return sum;
+    }
 
 };
+
+sumAll(5,6)
 
 // Do not edit below this line
 module.exports = sumAll;
