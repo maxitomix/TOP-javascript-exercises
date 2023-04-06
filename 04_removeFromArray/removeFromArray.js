2806598d0dca745dbaf0c98b71256dea39e1ae76
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-04-06 10:21:31 -0400

    palindromes done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -27,7 +27,7 @@ const removeFromArray = function(arrayInput,removeThis)
 
 
 
-    removeFromArray([1,2,3,4,5,6,7,8,9,10],3, 4, "hdh")
+    removeFromArray(["h","o","l","a",".","t","u",8,9,10],".", "-", ",")
 
 
     // const removeFromArray = function(arrayInput,removeThis) 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (inputString) 
+{
 
-};
+        let cleanInputString = inputString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+        cleanInputString = cleanInputString.toLowerCase();
+        console.log("This input", inputString, "is cleaned into: ")
+        console.log(cleanInputString)
+        console.log(". A new string with this length:",cleanInputString.length)
+        let reverseInputString = ""
+        for (i=(cleanInputString.length -1);i>=0;i-- )
+        {
+          reverseInputString += cleanInputString[i];
+        }
+        console.log(". We now reverse that string into: ",reverseInputString);
+        console.log(". We compare: ")
+        console.log(cleanInputString)
+        console.log(reverseInputString)
+        let isPalindrome = cleanInputString==reverseInputString
+        console.log(isPalindrome)
+        return isPalindrome;
+}
+
+palindromes("RaceCar")
 
 // Do not edit below this line
+
 module.exports = palindromes;
